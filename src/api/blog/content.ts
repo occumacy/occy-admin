862c47ffc5c3426713973c9e4791a300f7cf11fb
zhoux <350370222@qq.com>
2025-01-06 10:55:11 +0800

    Revert "feat: Occy Admin"
    This reverts commit 774534a257fbe9e6c85b2b17fa33c43e89ab661d.

--- src/api/blog/content.ts
+++
@@ -1,9 +0,0 @@
-import { RolePageParams, RolePageListGetResultModel } from '../blog/model/contentModel';
-import { defHttp } from '/@/utils/http/axios';
-
-enum Api {
-  RolePageList = '/blog/content/list',
-}
-
-export const getRoleListByPage = (params?: RolePageParams) =>
-  defHttp.get<RolePageListGetResultModel>({ url: Api.RolePageList, params });
--- src/locales/lang/zh-CN/routes/occy.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  blog: {
-    moduleName: '博客管理',
-    content: '内容管理',
-  },
-};
